7020cbb6a2221b7231fd3151f53f9bf67cb8382d
Miguel Silva <msilva0897@gmail.com>
2017-08-25 13:52:19 -0600

    Complete me

---
+++ lib/complete_me.rb
@@ -0,0 +1,28 @@
+class CompleteMe
+  attr_accessor :head
+
+  def initialize
+    @head = Node.new
+  end
+
+
+  def insert
+
+  end
+
+  def suggest
+
+  end
+
+  def count
+
+  end
+
+  def populate
+
+  end
+
+  def insert_words
+
+  end
+end
---
+++ lib/node.rb
@@ -0,0 +1,14 @@
+class Node
+  attr_accessor :word,
+                :letter,
+                :children
+  def initialize
+    @word = false
+    @letter = letter
+    @children = []
+  end
+
+
+
+
+end
