4743e472bb31faf840cb62d2ff37dd444c3b74e7
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-25 22:48:06 -0600

    add layout to tests

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -3,6 +3,34 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/complete_me'
 
+# Basic Specs:
+# Insert a single word to the autocomplete dictionary
+# Count the number of words in the dictionary
+# Populate a newline-separated list of words into the dictionary
+# Suggest completions for a substring
+# Mark a selection for a substring
+# Weight subsequent suggestions based on previous selections
+
 class TestCompleteMe < Minitest::Test
+
+  def test_insert_adds_word_to_dictionary
+
+  end
+
+  def test_count_returns_number_of_words_in_dictionary
+
+  end
+
+  def test_populate_adds_list_of_words_to_dictionary
+
+  end
+
+  def test_suggest_suggests_completions_for_substring
+
+  end
+
+  def test_select_adds_preferred_selection_to_begining_of_suggestions
+
+  end
 
 end
