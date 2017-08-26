955192a4f52f2861d5ade04ea85748e43299f539
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-26 15:43:54 -0600

    add select method and 3 helper methods

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -85,7 +85,33 @@ class CompleteMe
   end
 
   def select(prefix, selected_word)
-    
+    letters = split_word(prefix)
+    last = letters.length
+    prefix_node = prefix_finder(letters, last, @head)
+    prefix_node.search_node_for_prefix(prefix, selected_word)
+  end
+
+  def first_use_of_word(prefix, selected_word)
+    select[prefix] = [[1, selected_word]]
+  end
+
+  def search_for_word(prefix, selected_word)
+    word_exists = false
+    select[prefix].map! do |word|
+      if selected_word == word[1]
+        word[0] += 1
+        word_exists = true
+      end
+    end
+    first_use_of_word(prefix, selected_word) if !word_exists
+  end
+
+  def search_node_for_prefix(prefix, selected_word)
+    if select.has_key prefix
+      search_for_word(prefix, selected_word)
+    else
+      select[prefix] = [[1, selected_word]]
+    end
   end
 
   def count
